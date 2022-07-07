b321dd81b45f5a7bf0aee530ead9e0ff8f48f166
dangoth <alpha6667@gmail.com>
2022-07-07 19:10:11 +0200

    task 2

---
+++ TGL03/TGL03/Models/Square1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGL03.Models
+{
+    class Square1 : Rectangle
+    {
+        public Square1(double a) : base(a, a)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Square: {CalculatePerimeter()}, {CalculateArea()}";
+        }
+    }
+}
---
+++ TGL03/TGL03/Models/Square2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGL03.Models
+{
+    class Square2 : Figure
+    {
+        public double A { get; }
+
+        public Square2(double a)
+        {
+            A = a;
+        }
+        public override double CalculateArea() => Math.Pow(A, 2);
+
+        public override double CalculatePerimeter() => 4 * A;
+    }
+}
