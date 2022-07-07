eed57a1c454e7af4dd115a4296718f9eaa1752f4
dangoth <alpha6667@gmail.com>
2022-07-07 18:27:05 +0200

    initial commit

---
+++ geometry/geometry/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+interface Shape
+{
+    public double CalculateArea();
+}
+
+class Square : Shape
+{
+    private double x { get; set; }
+
+    public double CalculateArea() => x* x;
+}
+
+class Rectangle : Shape
+{
+    private double width { get; set; }
+    private double height { get; set; }
+
+    public double CalculateArea() => width * height;
+}
+
+class Triangle : Shape
+{
+    private double width { get; set; }
+    private double height { get; set; }
+
+    public double CalculateArea() => 0.5 * width * height;
+
+}
+
+class Circle : Shape
+{
+    private double radius { get; set; }
+
+    public double CalculateArea() => Math.PI * (radius * radius);
+}
